94ee6c1ae3b89094d8be23918193c9e52e2ff0b4
Junior7Marques <juniorjr07@live.com>
2023-07-21 14:21:20 -0300

    setting button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let button = document.querySelector('.btn');
-let projectUrl = "../projetos"
+let projectUrl = "https://junior7marques.github.io/portfolio/projetos/index.html";
+
+function newUrl(url) {
+    let win = window.open(url, '_parent');
+    win.focus();
+}
 
 button.addEventListener('click', () => {
-    
+    newUrl(projectUrl);
 })
